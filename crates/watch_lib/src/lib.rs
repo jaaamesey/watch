9fe7fd508cc70cd3b0e17ff2f6b944812e0719eb
James Karlsson <jameskarlsson1@gmail.com>
2025-12-13 17:40:58 +1100

    Think it's compatible with no std now?

--- crates/watch_lib/src/lib.rs
+++ crates/watch_lib/src/lib.rs
@@ -1,4 +1,183 @@
 #![no_std]
+extern crate alloc;
+use alloc::rc::Rc;
+use alloc::vec::Vec;
+use core::cell::RefCell;
+
+struct SignalData<T: Clone> {
+    value: T,
+    listeners: Vec<Rc<RefCell<dyn FnMut(T)>>>,
+}
+
+pub struct Signal<T: Clone> {
+    data: Rc<RefCell<SignalData<T>>>,
+}
+
+impl<T: Copy + PartialEq> Signal<T> {
+    pub fn set(&self, value: T) {
+        let prev = self.peek();
+        if value == prev {
+            return;
+        }
+        let mut data = self.data.borrow_mut();
+        data.value = value;
+        for i in 0..data.listeners.len() {
+            (data.listeners[i].borrow_mut())(value);
+        }
+    }
+    pub fn new(initial_value: T) -> Signal<T> {
+        Signal {
+            data: Rc::new(RefCell::new(SignalData {
+                value: initial_value,
+                listeners: Vec::new(),
+            })),
+        }
+    }
+}
+
+impl<T: Copy> Observable<T> for Signal<T> {
+    fn peek(&self) -> T {
+        self.data.borrow().value
+    }
+    fn subscribe<F: FnMut(T) + 'static>(&self, on_change: F) -> usize {
+        let mut data = self.data.borrow_mut();
+        data.listeners.push(Rc::new(RefCell::new(on_change)));
+        data.listeners.len() - 1
+    }
+    fn unsubscribe(&self, id: usize) {
+        // *self.listeners[id].borrow_mut() = || {};
+    }
+}
+
+pub fn derived<
+    Computation: Clone + PartialEq + 'static,
+    Compute: Fn(D0) -> Computation + 'static,
+    D0: Clone + Copy + 'static,
+    OD0: Observable<D0>,
+>(
+    dep0: &OD0,
+    compute: Compute,
+) -> DerivedSignal<Computation, D0, Compute> {
+    let ds = DerivedSignal {
+        data: Rc::new(RefCell::new(DerivedSignalData {
+            cache: None,
+            deps: dep0.peek(),
+            compute: compute,
+            listeners: Vec::new(),
+        })),
+    };
+    let ds_clone = ds.data.clone();
+    dep0.subscribe(move |new| {
+        let mut borrowed = ds_clone.borrow_mut();
+        borrowed.deps = new;
+        borrowed.maybe_recompute();
+    });
+    ds
+}
+
+pub fn derived2<
+    Computation: Clone + PartialEq + 'static,
+    Compute: Fn((D0, D1)) -> Computation + 'static,
+    D0: Clone + Copy + 'static,
+    D1: Clone + Copy + 'static,
+    OD0: Observable<D0>,
+    OD1: Observable<D1>,
+>(
+    deps: (&OD0, &OD1),
+    compute: Compute,
+) -> DerivedSignal<Computation, (D0, D1), Compute> {
+    let ds = DerivedSignal {
+        data: Rc::new(RefCell::new(DerivedSignalData {
+            cache: None,
+            deps: (deps.0.peek(), deps.1.peek()),
+            compute,
+            listeners: Vec::new(),
+        })),
+    };
+    let ds_clone = ds.data.clone();
+    deps.0.subscribe(move |new| {
+        let mut borrowed = ds_clone.borrow_mut();
+        borrowed.deps.0 = new;
+        borrowed.maybe_recompute();
+    });
+    let ds_clone = ds.data.clone();
+    deps.1.subscribe(move |new| {
+        let mut borrowed = ds_clone.borrow_mut();
+        borrowed.deps.1 = new;
+        borrowed.maybe_recompute();
+    });
+    ds
+}
+
+pub trait Observable<T> {
+    fn peek(&self) -> T;
+    fn subscribe<F: FnMut(T) + 'static>(&self, on_change: F) -> usize;
+    fn unsubscribe(&self, id: usize);
+}
+
+struct DerivedSignalData<
+    Derivation: Clone + PartialEq + 'static,
+    Deps: Clone + Copy + 'static,
+    F: Fn(Deps) -> Derivation + 'static,
+> {
+    cache: Option<Derivation>,
+    deps: Deps,
+    compute: F,
+    listeners: Vec<Rc<RefCell<dyn FnMut(Derivation)>>>,
+}
+
+pub struct DerivedSignal<
+    Derivation: Clone + PartialEq + 'static,
+    Deps: Clone + Copy + 'static,
+    F: Fn(Deps) -> Derivation + 'static,
+> {
+    data: Rc<RefCell<DerivedSignalData<Derivation, Deps, F>>>,
+}
+
+impl<T: Clone + PartialEq, Deps: Clone + Copy, F: Fn(Deps) -> T> Observable<T>
+    for DerivedSignal<T, Deps, F>
+{
+    fn peek(&self) -> T {
+        let mut data = self.data.borrow_mut();
+        if let Some(val) = &data.cache {
+            return val.clone();
+        }
+        data.cache = Some((data.compute)(data.deps));
+        data.cache.clone().unwrap()
+    }
+
+    fn subscribe<OnChange: FnMut(T) + 'static>(&self, on_change: OnChange) -> usize {
+        let mut data = self.data.borrow_mut();
+        data.listeners.push(Rc::new(RefCell::new(on_change)));
+        data.listeners.len() - 1
+    }
+
+    fn unsubscribe(&self, id: usize) {
+        //todo!()
+    }
+}
+
+impl<Derivation: Clone + PartialEq, Deps: Clone + Copy, F: Fn(Deps) -> Derivation>
+    DerivedSignalData<Derivation, Deps, F>
+{
+    pub fn maybe_recompute(&mut self) {
+        // TODO: do we need to check deps?
+        if self.listeners.is_empty() {
+            self.cache = None;
+            return;
+        }
+        let prev = self.cache.clone();
+        self.cache = Some((self.compute)(self.deps));
+        // TODO: have a way to skip this for large things?
+        if prev == self.cache {
+            return;
+        }
+        let new = self.cache.clone().unwrap();
+        for i in 0..self.listeners.len() {
+            (self.listeners[i].borrow_mut())(new.clone());
+        }
+    }
+}
 
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
